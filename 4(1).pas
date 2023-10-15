d2e291c756887d598019b2421e5d9a0377714760
Karriss <144677707+Karriss@users.noreply.github.com>
2023-10-15 18:14:10 +0300

    Add files via upload

---
+++ 4(1).pas
@@ -0,0 +1,16 @@
+﻿var a:array[1..30] of integer;
+i,s:integer;
+begin
+s:=0;
+randomize;
+for i:=1 to 30 do
+begin
+a[ i ]:=random(67)-99;
+write(a[ i ],' ');
+end;
+
+for i:=1 to 30 do
+if a [ i ]mod 2 = 0 then begin
+writeln (a [ i ]);
+end;
+end.
---
+++ 8(1).pas
@@ -0,0 +1,30 @@
+﻿const N = 15;
+var
+    arr: array[1..N] of byte;
+    i, k, num, frq , max_frq: byte;
+begin
+    randomize;
+    for i:=1 to N do begin
+        arr[i] := random(20);
+        write(arr[i],' ');
+    end;
+    writeln;
+
+    num := arr[1];
+    max_frq := 1;
+    for i:=1 to N-1 do begin
+        frq := 1;
+        for k:=i+1 to N do
+            if arr[i] = arr[k] then 
+                frq := frq + 1;
+        if frq > max_frq then begin
+            max_frq := frq;
+            num := arr[i];
+        end;
+    end;
+
+    if max_frq > 1 then
+        writeln(max_frq, ' раз(а) встречается число ', num)
+    else
+        writeln('Все элементы уникальны!');
+end.
