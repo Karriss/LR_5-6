f572e4d58c80e4cb6250f9a9958a2eb888e6bf7d
Karriss <144677707+Karriss@users.noreply.github.com>
2023-10-31 09:36:58 +0300

    Delete 6(1).pas

--- 6(1).pas
+++
@@ -1,8 +0,0 @@
-﻿var intArray : array[1..10] of integer; 
-i, n : integer; 
-begin 
-writeln('Введите 10 целых чисел: '); 
-for i := 1 to 10 do readLn(intArray[i]); 
-if intArray[1] > intArray[2] then write('Массив не является упорядоченным по возрастанию.') 
-else write('Массив упорядочен по возрастанию.'); 
-end.
